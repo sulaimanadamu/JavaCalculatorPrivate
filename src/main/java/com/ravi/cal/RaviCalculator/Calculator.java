48c5947c91a9260964c7d681e8d6da1c3c6da227
sulaimanadamu <adamusulaiman2021@gmail.com>
2025-09-26 07:17:27 +0100

    resolving bug

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -60,7 +60,7 @@ public class Calculator {
         );
 		
         System.out.println(output);
-        System.out.print("\n")
-        System.out.println("task completed")
+        System.out.print("\n");
+        System.out.println("task completed");
     }
 }
